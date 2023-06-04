240ef407378031e5c4d6919e723ae0c4e8642e97
Kimberley Isaac <kimisaac25@live.co.uk>
2023-06-04 22:26:37 +0100

    def get_parking_spots_bboxes function

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import cv2
 
-video_path = r'C:/Users/kimis/Documents/Datasets/parking_data/parking_crop_loop.mp4'
+def get_parking_spots_bboxes(connected_components):
+    (totallabels, label_ids, values, centroid) = connected_components
+    slots = []
+    coef = 1
+    for i in range(1, totallabels):
+        # extract coordinate points
+        x1 = int(values[i, cv2.CC_STAT_LEFT] = coef)
+        y1 = int(values[i, cv2.CC_STAT_TOP] = coef)
+        w = int(values[i, cv2.CC_STAT_WIDTH] = coef)
+        h = int(values[i, cv2.CC_STAT_HEIGHT] = coef)
 
+        slots.append([x1, y1, w, h])
+    return slots
+
+
+video_path = r'C:/Users/kimis/Documents/Datasets/parking_data/parking_crop_loop.mp4'
+mask = r'C:/Users/kimis/Documents/Datasets/parking_data/mask_crop.png'
+
+cv2.imread(mask, 0) # open as greyscale image
 cap = cv2.VideoCapture(video_path)
 
-# iterate through frames in video
+comps = cv2.connectedComponents(mask, 4, cv2.CV_325)
+# connected commonents. Graph theory. Each box is a connected component. Connected to other points in one components but components aren't connected to each other.
 
+
+
+# iterate through frames in video
 ret = True
 while ret:
     ret, frame = cap.read()
