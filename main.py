fcd3027ad16180287ecee75f6d72d52b17fd933e
Kimberley Isaac <kimisaac25@live.co.uk>
2023-06-06 20:26:29 +0100

    Get parking spot bounding box using mask

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 import cv2
 
 def get_parking_spots_bboxes(connected_components):
+    # grabs each component and exctracts bounding box
     (totallabels, label_ids, values, centroid) = connected_components
     slots = []
     coef = 1
     for i in range(1, totallabels):
         # extract coordinate points
-        x1 = int(values[i, cv2.CC_STAT_LEFT] = coef)
-        y1 = int(values[i, cv2.CC_STAT_TOP] = coef)
-        w = int(values[i, cv2.CC_STAT_WIDTH] = coef)
-        h = int(values[i, cv2.CC_STAT_HEIGHT] = coef)
+        x1 = int(values[i, cv2.CC_STAT_LEFT] * coef)
+        y1 = int(values[i, cv2.CC_STAT_TOP] * coef)
+        w = int(values[i, cv2.CC_STAT_WIDTH] * coef)
+        h = int(values[i, cv2.CC_STAT_HEIGHT] * coef)
 
         slots.append([x1, y1, w, h])
     return slots
 
 
 video_path = r'C:/Users/kimis/Documents/Datasets/parking_data/parking_crop_loop.mp4'
-mask = r'C:/Users/kimis/Documents/Datasets/parking_data/mask_crop.png'
+mask_path = r'C:/Users/kimis/Documents/Datasets/parking_data/mask_crop.png'
 
-cv2.imread(mask, 0) # open as greyscale image
+mask = cv2.imread(mask_path, 0) # open as greyscale image
 cap = cv2.VideoCapture(video_path)
 
-comps = cv2.connectedComponents(mask, 4, cv2.CV_325)
+comps = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
 # connected commonents. Graph theory. Each box is a connected component. Connected to other points in one components but components aren't connected to each other.
 
+# gives bounding box for each parking spot as list of coords
+spots = get_parking_spots_bboxes(comps)
 
+print(spots[0])
 
 # iterate through frames in video
 ret = True
