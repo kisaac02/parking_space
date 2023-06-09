bd1fdd51f4e2901c35b77835d1ed28cb77bb5ace
Kimberley Isaac <kimisaac25@live.co.uk>
2023-06-09 20:47:36 +0100

    SVM model train, test, save

--- model.py
+++ model.py
@@ -2,9 +2,10 @@ import os
 import numpy as np
 from skimage.io import imread
 from skimage.transform import resize
-from sklearn.model_selection import train_test_split
+from scikit-learn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 # TODO move image processing onto GPU. Use CUDA or 
 input_dir = r'C:/Users/kimis/Documents/Datasets/parking_data/clf-data'
@@ -39,6 +40,11 @@ grid_search = GridSearchCV(classifier, parameters)
 grid_search.fit(x_train, y_train)
 
 # test
+best_estimator = grid_search.best_estimator_
 
+y_predict = best_estimator.predict(x_test)
 
+score = accuracy_score(y_predict, y_test)
+
+print(f'{score*100}% of samples where correctly classified')
 
